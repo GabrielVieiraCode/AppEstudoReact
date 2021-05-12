b3048bbc1c2885599c500c912ac84d595256cc4c
gabrielvieiracode <gabriel.vieira.trabalho@gmail.com>
2021-05-12 18:11:40 -0300

    app react

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react'
+import Table from './Table'
+import Form from './Forms'
+
+class App extends Component {
+    state = {
+        characters: []
+    };
+
+    render() {
+        const { characters } = this.state;
+        return (
+            <div className="container">
+                <Table characterData={characters} removeCharacter={this.removeCharacter} />
+                <Form handleSubmit={this.handleSubmit} />
+
+            </div>
+        )
+    }
+    handleSubmit = (character) => {
+        this.setState({characters: [...this.state.characters, character]})
+      }
+}
+
+export default App
